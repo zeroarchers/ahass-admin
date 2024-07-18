6d9e4150d6de5594f80f5da2277c207043b9f646
mozaldy <moxaldy@gmail.com>
2024-07-18 20:42:32 +0700

    - Initialized next.js project with `pnpx create-next-app` - Initialiazed shadcn-ui and aceternity - Created landing page and hero section - Created login page

---
+++ app/login/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Page() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Masukkan email anda untuk masuk ke sistem admin ahass
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="nama@email.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:flex">
+        <Image
+          src="/logo_ahass.png"
+          alt="Image"
+          width="1080"
+          height="720"
+          className="w-[90%] mt-auto m-auto object-center dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
